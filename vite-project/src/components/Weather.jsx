18dda60f7571061ed0057589392b2463234ab628
bia <biatorresdacosta@gmail.com>
2025-03-22 01:51:06 -0300

    Quarto commit

--- vite-project/src/components/Weather.jsx
+++ vite-project/src/components/Weather.jsx
@@ -1,66 +1,127 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import './Weather.css'
-import search_icon from '../assets/search.png';
-import clear_icon from '../assets/clear.png';
-import cloud_icon from '../assets/cloud.png';
-import drizzle_icon from '../assets/drizzle.png';
-import rain_icon from '../assets/rain.png';
-import snow_icon from '../assets/snow.png';
-import wind_icon from '../assets/wind.png';
-import humidity_icon from '../assets/humidity.png';
+import search_icon from '../assets/search.png'
+import clear_icon from '../assets/clear.png'
+import cloud_icon from '../assets/cloud.png'
+import drizzle_icon from '../assets/drizzle.png'
+import humidity_icon from '../assets/humidity.png'
+import rain_icon from '../assets/rain.png'
+import snow_icon from '../assets/snow.png'
+import wind_icon from '../assets/wind.png'
 
+
+//deu problema com a API key 
 
 const Weather = () => {
 
-  const search = async (city) =>{
-    try {
+  const inputRef = useRef()
 
-      const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${import.meta.env.VITE_APP_ID}`
-      //PESQUISAR O QUE É IMPORT.META.ENV.NOMEDAVARIAVEL
-      
-      const response = await fetch(url);
-      const data = await response.json();
-      console.log(data)
+  const [weatherData, setWeatherData] = useState(false);
 
-    } catch (error) {
-      
-    }
+  const allIcons = {
+    '01d': clear_icon,
+    '01n': clear_icon,
+    '02d': cloud_icon,
+    '02n': cloud_icon,
+    '03d': cloud_icon,
+    '03n': cloud_icon,
+    '04d': drizzle_icon,
+    '04n': drizzle_icon,
+    '09d': rain_icon,
+    '09n': rain_icon,
+    '10d': rain_icon,
+    '10n': rain_icon,
+    '13d': snow_icon,
+    '13n': snow_icon,
   }
 
+  console.log('APiiiiiii teste:', import.meta.env.VITE_APP_ID);
 
 
+    const search = async (city) => {
 
-  return (
+      console.log('API Key carregada:', import.meta.env.VITE_APP_ID);
+      console.log('Cidade digitada:', city);
+
+      if(city==='' ){
+        alert('Digite o nome da cidade por favor');
+        return; // PRA QUE PRECISA DESSE RETURN?????????????????????????????????????????
+      }
+      try {
+
+        console.log('API Key:', import.meta.env.VITE_APP_ID);
+
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${import.meta.env.VITE_APP_ID}`;
+
+        console.log('URL da requisição:', url);
+
+        const response = await fetch(url);
+        const data = await response.json();
+
+        console.log('Resposta da API:', data);
+
+
+        if(!response.ok){
+          alert(data.message);
+          return;
+        }
+
+        setWeatherData({
+          humidity: data.main.humidity,
+          windSpeed: data.wind.speed,
+          temperature: Math.floor(data.main.temp), //nao entendi esse math.floor
+          location: data.name,
+          icon: allIcons[data.weather[0].icon] ||clear_icon
+        });
+
+      } catch (error) {
+        console.error('Erro ao buscar os dados do clima:', error);
+
+        setWeatherData(false);
+      //  console.error('Error in fetching weather data');
+        
+      }
+    }
+
+      useEffect (()=> {
+        search('London');
+      }, [])
+
+  return ( //verificar se o <p> da mensagem aparece
+
     <div className='weather'>
-
       <div className="search-bar">
-        <input type="text" placeholder='Pesquisar...' name="" id="" />
-        <img src={search_icon} alt="" />
+        <input ref={inputRef} type="text" placeholder='Search...' />
+        <img src={search_icon} alt="" onClick={() => search(inputRef.current.value)} />
       </div>
-   
-      <img src={clear_icon} className='weather-icon' />
-      <p className='temperature'>16ºC</p>
-      <p className='location'>London</p>
-
+      {weatherData?<>
+      
+      <img src={weatherData.icon} alt="" className='weather-icon' />
+      <p className='temperature'>{weatherData.temperature}ºC</p>
+      <p className='location'>{weatherData.location}</p>
+      <p id="mensagem"></p> 
       <div className="weather-data">
-
-        <div className="col">
-          <img src={humidity_icon} />
-          <div>
-            <p>91%</p>
-            <span>Humidity</span>
-          </div>
+       <div className="col"> 
+        <img src={humidity_icon} alt="" />
+        <div>
+          <p>{weatherData.humidity}%</p>
+          <span>Humidity</span>
         </div>
+       </div>
 
-        <div className="col">
-          <img src={wind_icon} />
-          <div>
-            <p>3.6</p>
-            <span>Wind Speed Km/h</span>
-          </div>
+       <div className="col">
+        <img src={wind_icon} alt="" />
+        <div>
+          <p>{weatherData.windSpeed} km/hour</p>
+          <span>Wind Speed</span>
         </div>
+       </div>
+
 
       </div>
+
+      </>: <></>}
+      
     </div>
   )
 }
